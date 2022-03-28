c76aa505dbcef96009fc3a214a07dfae42267ed8
Sridhara T V <35601862+sridharatv@users.noreply.github.com>
2022-03-28 20:51:56 +0530

    Update deploy.sh
    updated kubectl

--- fullstack/kubernetes/deploy.sh
+++ fullstack/kubernetes/deploy.sh
@@ -6,7 +6,7 @@ sudo ./aws/install
 pip3 install --upgrade --user awscli
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-curl -o kubectl curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/darwin/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 export AWS_DEFAULT_REGION=us-east-1
