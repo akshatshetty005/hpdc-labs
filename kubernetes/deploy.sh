1e907de62da33b4aa226b5e10864bcd8d2e90a2d
EC2 Default User <ec2-user@ip-172-31-2-170.ec2.internal>
2022-11-10 19:08:26 +0000

    update

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,7 +6,7 @@ sudo ./aws/install
 pip3 install --upgrade --user awscli
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-curl -o curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/darwin/amd64/kubectl
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 export AWS_DEFAULT_REGION=us-east-1
