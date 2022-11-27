d159d569c02489aebe932db5958ce2396dba2a5c
Sridhara T V <35601862+sridharatv@users.noreply.github.com>
2022-11-27 23:38:39 +0530

    Update api.js
    reset vars

--- fullstack/frontend/code/api.js
+++ fullstack/frontend/code/api.js
@@ -1,36 +1,36 @@
 var dumpproduct = {
-    "API": "http://loadbalancer-1389723745.us-east-1.elb.amazonaws.com/api/v1.0/dumpproduct",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/dumpproduct",
     "type": "GET"
 }
 var getallproduct = {
-    "API": "http://loadbalancer-1389723745.us-east-1.elb.amazonaws.com/api/v1.0/getallproduct",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/getallproduct",
     "type": "GET"
 }
 var getproductbyid = {
-    "API": "http://loadbalancer-1389723745.us-east-1.elb.amazonaws.com/api/v1.0/getproductbyid",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/getproductbyid",
     "type": "POST"
 }
 var addtocart = {
-    "API": "http://ad5ede698b6844c4a8d727b8deb5c625-181456002.us-east-1.elb.amazonaws.com/api/v1.0/addtocart",
+    "API": "http://CARTENDPOINT/api/v1.0/addtocart",
     "type": "POST"
 }
 var getcart = {
-    "API": "http://ad5ede698b6844c4a8d727b8deb5c625-181456002.us-east-1.elb.amazonaws.com/api/v1.0/getcart",
+    "API": "http://CARTENDPOINT/api/v1.0/getcart",
     "type": "POST"
 }
 
 var checkoutAPI = {
-    "API": "https://x7u5p31vm4.execute-api.us-east-1.amazonaws.com/api/checkout",
+    "API": "https://APIGWURL/api/checkout",
     "type": "POST"
 }
 
 var confirmOrder = {
-    "API": "https://checkout.devloc.archeplay.com/api/v1.0/confirmorder",
+    "API": "https://APIGWURL/v1.0/confirmorder",
     "type": "POST"
 }
 
 var getOrder = {
-    "API": "https://checkout.devloc.archeplay.com/api/v1.0/getorder",
+    "API": "https://APIGWURL/v1.0/getorder",
     "type": "POST"
 }
 
