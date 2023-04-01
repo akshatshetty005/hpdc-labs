006aaacb59d1debc5f5dc32e1a92e4cd5695b45a
Sridhara T V <35601862+sridharatv@users.noreply.github.com>
2023-04-01 23:15:26 +0530

    Update api.js

--- fullstack/frontend/code/api.js
+++ fullstack/frontend/code/api.js
@@ -1,21 +1,21 @@
 var dumpproduct = {
-    "API": "http://loadbalancer-1807613186.us-east-1.elb.amazonaws.com/api/v1.0/dumpproduct",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/dumpproduct",
     "type": "GET"
 }
 var getallproduct = {
-    "API": "http://loadbalancer-1807613186.us-east-1.elb.amazonaws.com/api/v1.0/getallproduct",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/getallproduct",
     "type": "GET"
 }
 var getproductbyid = {
-    "API": "http://loadbalancer-1807613186.us-east-1.elb.amazonaws.com/api/v1.0/getproductbyid",
+    "API": "http://PRODUCTENDPOINT/api/v1.0/getproductbyid",
     "type": "POST"
 }
 var addtocart = {
-    "API": "http://a284238e1d1284b1dabf3b2635d25867-1044347556.us-east-1.elb.amazonaws.com/api/v1.0/addtocart",
+    "API": "http://CARTENDPOINT/api/v1.0/addtocart",
     "type": "POST"
 }
 var getcart = {
-    "API": "http://a284238e1d1284b1dabf3b2635d25867-1044347556.us-east-1.elb.amazonaws.com/api/v1.0/getcart",
+    "API": "http://CARTENDPOINT/api/v1.0/getcart",
     "type": "POST"
 }
 
