55b053b4f338b197ce7be354574ca719ed200588
Sridhara T V <35601862+sridharatv@users.noreply.github.com>
2023-04-11 10:14:59 +0530

    Add files via upload

---
+++ ha-web-app/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Insalling pre-requisites"
+sudo pip install boto boto3 botocore
+sudo yum -y install ansible
+ansible-galaxy collection install community.aws
+
+bkt_name="three-tier-web-app-`uuidgen`"
+bkt_lst=$( aws s3 ls | cut -d " " -f3 | grep "ecomm-app" | head -n 1 )
+echo $bkt_lst
+if [ $bkt_lst ]; then
+	echo "Bucket already exists $bkt_lst"
+	bkt_name=$bkt_lst
+else
+	echo "Creating S3 bucket $bkt_name"
+	aws s3 mb s3://$bkt_name
+fi
+
+echo "Synching files to S3 bucket $bkt_name"
+aws s3 sync . s3://$bkt_name
+
+export S3_Bucket="$bkt_name" | tee -a ~/.bash_profile
+
