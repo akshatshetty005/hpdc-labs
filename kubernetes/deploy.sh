85cfdaecbac8c58cf860a690008254dbf338cbcc
Sridhara T V <35601862+sridharatv@users.noreply.github.com>
2023-12-04 14:28:56 +0530

    Update deploy.sh version of eksctl

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,14 +6,13 @@ sudo ./aws/install
 pip3 install --upgrade --user awscli
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.7/2023-11-14/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 export AWS_DEFAULT_REGION=us-east-1
 eksctl create cluster \
 --region us-east-1 \
 --name prod \
---version 1.23 \
 --nodegroup-name standard-workers \
 --node-type t3a.medium \
 --zones=us-east-1a,us-east-1b,us-east-1c \
